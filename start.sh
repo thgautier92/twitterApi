f25a05510c00489ade8271d47c5420a2a3e844f8
Thierry GAUTIER <th.gautier92@gmail.com>
2017-06-26 18:35:27 +0200

    update project

--- start.sh
+++ start.sh
@@ -3,4 +3,7 @@ echo Analyse des comptes TWITTER
 echo ==================================================
 node callTwitter.js gautier,PARIS
 node callTwitter.js mallet,saint%20martin%20de%20ré
-node callTwitter.js mallet, asnieres
+node callTwitter.js mallet,asnieres
+node callTwitter.js sma,PARIS
+node callTwitter.js smabtp,PARIS
+node callTwitter.js anabelle%20grandjean
